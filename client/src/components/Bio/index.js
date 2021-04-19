906bf5da1ed711592b023eb79dcc9e857903a52b
Myka Kugaya <mykakug129@gmail.com>
2021-04-18 18:25:44 -0700

    bio changes

--- client/src/components/Bio/index.js
+++ client/src/components/Bio/index.js
@@ -11,9 +11,12 @@ function Bio() {
           <Col xs={12} sm={9} className="bioCol">
             <h1 className="aboutmeTitle">About Me</h1>
             <p className="biop">
-            Hi, my name is Adelina Kugaya and I am a senior at Palos Verdes Peninsula High School. I have been practicing ceramics for over 5 years, focusing on creating on the potter’s wheel. I have learning experience in Okayama, Japan, where potters create traditional Bizen-style ceramics. I had the opportunity to assist through the firing process using a noborigama, or step-climbing kiln. 
-            More recently, I have experimented with sculpting head figures. Visible on the home page, I am producing clay sculptures of Asians among us that testify to the resilience of my race. My ongoing sculpture series combines gentle and elegant beauty with violent and degrading horror to signify the terror that comes with Asian American struggles.
-            </p>
+            My name is Adelina Kugaya and I am a senior at Palos Verdes Peninsula High School. I was introduced to ceramics in the seventh grade, producing pieces for the fun of crafting. I fell in love with the feeling of complete freedom when expressing creativity using clay. I soon found a teacher who rented out an art studio to me and since then, I have been practicing ceramics for over 5 years, focusing on throwing on the potter’s wheel. Around 3 years ago, feeling frustrated with my lack of time spent crafting, I purchased a potter’s wheel for my home. I was able to throw my pieces at home with unlimited time, then bring the finished products to the studio for firing. 
+            <br></br>
+            I also have learning experience in Okayama, Japan, where potters create traditional Bizen-style ceramics. I had the opportunity to assist through the firing process using a noborigama, or step-climbing kiln. My pieces have ranged from small cups and vases to large ceremonial platters, many being inspired by the traditional craftsmanship of Japan.
+<br></br>
+More recently, I have experimented with sculpting head figures. Visible on the home page, I am producing clay sculptures of Asians among us that testify to the resilience of my race. My ongoing sculpture series combines gentle and elegant beauty with violent and degrading horror to signify the terror that comes with Asian American struggles.
+</p>
           </Col>  
           <Col xs={12} sm={3} className="imgCol">
             <img src={ProfilePic} className="bioimage" alt="bioimage"/>
--- client/src/components/Mission/index.js
+++ client/src/components/Mission/index.js
@@ -14,10 +14,14 @@ export default function Mission() {
                 <Col xs={12} sm={8} md={8} lg={8} className="missionCol">
                     <p className="missionp">
                     In light of recent assaults on Asian Americans, the goal of CERAMIX is to form a bridge between polarized parties, Asian culture and American culture, using art as a medium. 
-                    How?
-                    CERAMIX provides a platform for unknown traditional Asian craftsmanship and its practitioners who are not financially able to gain global exposure. Including Asian artists practicing traditional Bizen and Onta pottery, CERAMIX aims to educate people on the deep-rooted culture that Asian artists can offer on a global scale.
-                    By showcasing the efforts and talent of Asian artists, CERAMIX communicates equality for all people.
+<br></br>
+How?
 
+CERAMIX provides a platform for unknown traditional Asian craftsmanship and its practitioners who are not financially able to gain global exposure. CERAMIX aims to educate people on the deep-rooted culture that Asian artists can offer on a global scale.
+
+By showcasing the efforts and talent of Asian artists, CERAMIX communicates equality for all people.
+
+As CERAMIX becomes a public platform, it will allow any artist to submit pieces that relate to the theme of the website, mixing vast art cultures.
                     </p>
                 </Col>  
             </Row>
